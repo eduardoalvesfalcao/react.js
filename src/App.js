1a8e9e274a859b1db8dd7d29af81eab8810f505d
Eduardo Alves Falcão <edualvesfalcao@gmail.com>
2022-06-13 18:16:40 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Exercicio01 from "./components/exercicio01";
 
-function App() {
+const App = () => {
+  let num1 = 5;
+  let num2 = 5;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <Exercicio01 num1={num1} num2={num2}/>
+      </div>
+    );
 }
-
 export default App;
